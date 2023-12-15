5a064a3b6595571999bb6087b3c2dd71ffa9bae7
Lachlan Findlater <psyentist@madpsyence.com>
2023-12-15 21:28:49 +0000

    day2 completed

--- day2/day2.lua
+++ day2/day2.lua
@@ -6,44 +6,55 @@ function Get_input()
     return lines
 end
 
-function split (input, sep)
-    if sep == nil then
-            sep = "%s"
-    end
-    local t={}
-    for str in string.gmatch(input, "([^"..sep.."]+)") do
-            table.insert(t, str)
-    end
-    return t
-end
-
 function delete_game_id(line)
     return string.sub(line, string.find(line, ":")+2, #line)
 end
 
-Cubes = {"red", "blue", "green", red = 12, green = 13, blue = 14}
+function game_iter(game)
+    local rounds = string.gmatch(delete_game_id(game), "[%d%a]+")
+    return function()
+        for seg in rounds do
+            return {seg, rounds()}
+        end
+        return nil
+    end
+end 
 
-function trim(s)
-    return (s:gsub("^%s*(.-)%s*$", "%1"))
-end
-
-function cube_count(round, colour)
-    for key, group in pairs(split(round, ",")) do
-        if group:find(colour) ~= nil then
-            return tonumber(split(trim(group), " ")[1])
+function part1()
+    local line = 0
+    local sum = 0
+    local Cubes = {red = 12, blue = 14, green = 13}
+    for k,game in pairs(Get_input()) do
+        local valid = true
+        line = line + 1
+        for round in game_iter(game) do
+            -- print(round[1] .. " " .. round[2] .. " = " .. Cubes[round[2]])
+            if tonumber(round[1]) > Cubes[round[2]] then
+                valid = false
+                break
+            end
+        end
+        if valid == true then
+            print(line)
+            sum = sum + line
         end
     end
-    return 0
+    return sum
 end
 
-function validate_game_round(round)
-
-end
-
-for key, line in pairs(Get_input()) do
-    print(delete_game_id(line))
-    for key,game in pairs(split(delete_game_id(line), ";")) do
-            split(game, ",")
+function part2()
+    local sum = 0
+    for k,game in pairs(Get_input()) do
+        local Cubes = {red = 0, blue = 0, green = 0}
+        for round in game_iter(game) do
+            if tonumber(round[1]) > Cubes[round[2]] then
+                Cubes[round[2]] = tonumber(round[1])
+            end
+        end
+        sum = sum + (Cubes["red"] * Cubes["green"] * Cubes["blue"])
     end
+    return sum
 end
 
+print("part 1: " .. part1())
+print("part 2: " .. part2())
