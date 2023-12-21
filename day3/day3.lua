17505046cb98d825682f1ab9dbe37c30e0d26483
Lachlan Findlater <psyentist@madpsyence.com>
2023-12-21 10:09:56 +0000

    day 3 part2 fin

--- day3/day3.lua
+++ day3/day3.lua
@@ -40,4 +40,43 @@ function part1()
     print("part1: " .. sum)
 end
 
+function find_adjacent_numbs(nLine, nCol)
+    local i, numbs = -1, {}
+    while i < 2 do
+        local nColStart, nColEnd = input[nLine + i]:find("[%d]+")
+        while nColEnd ~= nil do
+            if (nColStart <= lineClamp(nCol + 1) and nColStart >= lineClamp(nCol - 1)) 
+            or (nColEnd <= lineClamp(nCol + 1) and nColEnd >= lineClamp(nCol - 1)) then
+                numbs[#numbs + 1] = tonumber(input[nLine + i]:sub(nColStart, nColEnd))
+            end
+            nColStart, nColEnd = input[nLine + i]:find("[%d]+", nColEnd + 1)
+        end
+        i = i + 1
+    end
+    return numbs
+end
+
+function merge(table1, table2)
+    for k,v in ipairs(table2) do
+        table1[#table1 + k] = v
+    end
+    return table1
+end
+
+function part2()
+    local sum = 0
+    for i,line in ipairs(input) do
+        local nColStart, nColEnd = line:find("*")
+        while nColEnd ~= nil do
+            local numbs = find_adjacent_numbs(i,nColStart)
+            if numbs ~= nil and #numbs == 2 then
+                sum = sum + (numbs[1] * numbs[2])
+            end
+            nColStart, nColEnd = line:find("*", nColEnd + 1)
+        end
+    end
+    print("part 2: " .. sum)
+end
+
 part1()
+part2()
